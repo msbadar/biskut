1366bbedcb832126aec131f3b81fcd3610fb578e
msbadar <sulochana8486@gmail.com>
2019-07-31 02:09:45 +0530

    fix credential issues

--- reduxMiddleware.js
+++ reduxMiddleware.js
@@ -1,23 +1,24 @@
-export default ( {url, headers={}}) => {
+export default ({ url, headers = {} }) => {
     return store => next => async action => {
         if (!action.xhr) return next(action);
-            const options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                mode: 'cors',
-                cache: 'default',
-                body: JSON.stringify(action),
-                ...headers
-            };
-            try {
-                next(action)
-                const response = await fetch(url, options).then(r => r.json());
-                response.type = response.type || "HTTP_ERROR";
-                store.dispatch(response)
-            } catch (e) {
-                store.dispatch({ type: "NETWORK_ERROR", e })
-            }
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers,
+            },
+            credentials: 'include',
+            mode: 'cors',
+            cache: 'default',
+            body: JSON.stringify(action),
+        };
+        try {
+            next(action)
+            const response = await fetch(url, options).then(r => r.json());
+            response.type = response.type || "HTTP_ERROR";
+            store.dispatch(response)
+        } catch (e) {
+            store.dispatch({ type: "NETWORK_ERROR", e })
+        }
     }
 }
